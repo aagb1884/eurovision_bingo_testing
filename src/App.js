deb0fc412cff8d5b35690a24aa374463086521d2
Andrew Blair <a.a.g.blair@gmail.com>
2023-12-15 16:06:31 +0000

    initial cypress tests added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import HomePage from './Components/Home';
+
+
+function App() {
+
+  return (
+    <div className="app">
+   < HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Button.js
@@ -0,0 +1,32 @@
+const Button = ({ cliche, isActive, setIsActive, numberOfActiveButtons, setNumberOfActiveButtons, checkIfBingoButtonIsActive}) => {
+
+    const handleClick = () => {
+        setIsActive(!isActive);
+        addActiveButtons();
+        checkIfBingoButtonIsActive();
+        console.log(numberOfActiveButtons)
+      };
+
+    const addActiveButtons = () => {
+      if (!isActive && numberOfActiveButtons <= 6) {
+        setNumberOfActiveButtons(numberOfActiveButtons += 1)
+      } 
+      else {setNumberOfActiveButtons(0)}
+    }
+
+    return (
+        <div className={`grid-item ${isActive ? 'active' : ''}`}>
+        <button
+          onClick={handleClick}
+          className={`euro-btn ${isActive ? 'active' : ''}`}
+          type="button"
+          data-testid="card-button"
+        >
+          {cliche}
+        </button>
+      </div>
+
+    );
+}
+
+export default Button;
