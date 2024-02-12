c6b573145fab8e81b6cd31b8077bf3824a0be83b
Andrew Blair <a.a.g.blair@gmail.com>
2024-02-12 15:47:07 +0000

    fixing yaml file

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,7 +5,7 @@ const HomePage = () => {
       <div className="eurovision-bingo-landing">
         
         
-          <header className="animate-charcter">
+          <header className="animate-character">
             <div className="animated-header">EUROVISION BINGO</div>
           </header>
 
